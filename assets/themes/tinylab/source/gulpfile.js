5561425a7d5fe0327f11b5bba424a3880e5a28ef
Peng Jingwen <pengsrc@icloud.com>
2015-10-08 20:34:53 +0800

    Setup Theme
    Signed-off-by: Peng Jingwen <pengsrc@icloud.com>

---
+++ assets/themes/tinylab/source/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp      = require('gulp'),
+    sass      = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css'),
+    rename    = require("gulp-rename");
+
+gulp.task('default', ['watch']);
+
+var buildStyleSheets = function() {
+    gulp.src('./stylesheets/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('../build/'))
+        .pipe(minifyCSS())
+        .pipe(rename(function(path) {
+            path.extname = '.min.css'
+        }))
+        .pipe(gulp.dest('../build/'));
+};
+
+gulp.task('watch', function() {
+    buildStyleSheets();
+    gulp.watch(['stylesheets/*.scss', 'stylesheets/components/*.scss'], function(event) {
+        console.log(`File ${event.path} was ${event.type} running tasks...`);
+        buildStyleSheets();
+    });
+});
